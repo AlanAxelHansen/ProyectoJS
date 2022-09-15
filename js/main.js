a7bb748f5f61e92f1c9df2b6f2f959ba381b9d85
Alan Hansen <alanhansenaxel@hotmail.com>
2022-09-14 22:07:45 -0300

    Modo oscuro añadido y saque los Prompts para modificar a futuro

--- js/main.js
+++ js/main.js
@@ -1,18 +1,41 @@
+//Cambiar a Modo oscuro/claro
+const btnSwitch = document.querySelector('#switch');
+
+btnSwitch.addEventListener('click', () => {
+	document.body.classList.toggle('dark');
+	btnSwitch.classList.toggle('active');
+
+	// Guardamos el modo en localstorage.
+	if(document.body.classList.contains('dark')){
+		localStorage.setItem('dark-mode', 'true');
+	} else {
+		localStorage.setItem('dark-mode', 'false');
+	}
+});
 
 
-function pedirNombre() {
-    alert("¡Bienvenido a Dados Selk!")
-    let nombre = prompt("¡Hola! ¿Quien va a tirar? ");
-    while (nombre === "") {
-        nombre = prompt("Identificate, queremos saber quien es el ludopata ");
-    }
-    return nombre;
+if(localStorage.getItem('dark-mode') === 'true'){
+	document.body.classList.add('dark');
+	btnSwitch.classList.add('active');
+} else {
+	document.body.classList.remove('dark');
+	btnSwitch.classList.remove('active');
 }
 
-let bonusDado;
-let dadoElejido;
-let bonus = 0;
-let result;
+// function pedirNombre() {
+//     alert("¡Bienvenido a Dados Selk!")
+//     let nombre = prompt("¡Hola! ¿Quien va a tirar? ");
+//     while (nombre === "") {
+//         nombre = prompt("Identificate, queremos saber quien es el ludopata ");
+//     }
+//     return nombre;
+// }
+
+let bonusDado,
+dadoElejido,
+bonus = 0,
+result,
+Dadaso;
 
 const Dado = [
     {
@@ -52,36 +75,49 @@ const Dado = [
      },
 ];
 
-let Dadaso;
-
-function eleccionDado(punto) {
-    
-        punto = parseInt(prompt("Agarra el dado : \n0)D100\n1)D20\n2)D12\n3)D10\n4)D8\n5)D6\n6)D4 "));
-    
-        Dadaso = punto;
-
-    const o = Dado.find(valor => {
-        valor.id == punto;
-    });
-
-    console.log(Dado[punto]);
-    console.log(punto);
+function seleccionarDado(){
+   let dadito = document.getElementsByName('dadito');
+   let dado = dadito.value;
+   console.log(dado)
+   document.getElementByName('dadito').innerText = `Ud. ha seleccionado el lenguaje ${dado}.`;
 };
 
-function resultado() {
-    bonus = parseInt(prompt("¿De cuanto es el bonus? "));
+// function seleccionarDado() {
+//    punto = document.getElementsById('opcionesDado');
+//    Dadaso = punto.value;
+//    console.log(Dadaso, punto);
+
+// };
+
+
+// function eleccionDado(punto) {
     
-    result = (Math.floor(Math.random () * Dado[Dadaso].numero + 1));
-};
+//         punto = parseInt(prompt("Agarra el dado : \n0)D100\n1)D20\n2)D12\n3)D10\n4)D8\n5)D6\n6)D4 "));
+    
+//         Dadaso = punto;
 
-function totalDado() {
-    alert("Tu resultado es: " + result + "+" + bonus);
-    let juan = (result + bonus);
-    alert("La suma de ambos es: " + (juan))
-};
+//     const o = Dado.find(valor => {
+//         valor.id == punto;
+//     });
+
+//     console.log(Dado[punto]);
+//     console.log(punto);
+// };
+
+// function resultado() {
+//     bonus = parseInt(prompt("¿De cuanto es el bonus? "));
+    
+//     result = (Math.floor(Math.random () * Dado[Dadaso].numero + 1));
+// };
+
+// function totalDado() {
+//     alert("Tu resultado es: " + result + "+" + bonus);
+//     let juan = (result + bonus);
+//     alert("La suma de ambos es: " + (juan))
+// };
 
 
-let usuario = pedirNombre();
-let miDado = eleccionDado(Dadaso);
-let Bonus = resultado();
-totalDado();
+// // let usuario = pedirNombre();
+// // let miDado = eleccionDado(Dadaso);
+// let Bonus = resultado();
+// totalDado();
